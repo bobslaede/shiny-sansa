373e85bdb50e16e01e118382dfdcd50727f4e17d
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-01-15 20:27:00 +0100

    making a rest server real quick

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var restify = require('restify');
-var host = process.env.HOST || '127.0.0.1';
 var port = process.env.PORT || '3007';
 
 
@@ -12,20 +11,22 @@ var server = restify.createServer({
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.use(function logger(req,res,next) {
-  console.log(new Date(),req.method,req.url);
+server.use((req,res,next) => {
+  console.log(new Date(), req.method, req.url);
   next();
+});
+
+server.get('/', (req, res) => {
+  res.json(server.name);
 });
 
 server.get('/me', (req, res) => {
   res.json({name:'foo'})
-})
+});
 
-server.on('uncaughtException',function(request, response, route, error){
+server.on('uncaughtException',(request, response, route, error) => {
   console.error(error.stack);
   response.send(error);
 });
 
-server.listen(port, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(port);
