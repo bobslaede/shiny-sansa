3d694ab2569b99523b311cd1a0066c2b89a636e5
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-01-15 20:23:30 +0100

    making a rest server real quick

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-'use strict'; 
-let foo = [1,2].map(x => x * 2);
-console.log(foo);
+'use strict';
+
+var restify = require('restify');
+var host = process.env.HOST || '127.0.0.1';
+var port = process.env.PORT || '3007';
+
+
+var server = restify.createServer({
+  name: 'Things API Server'
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.use(function logger(req,res,next) {
+  console.log(new Date(),req.method,req.url);
+  next();
+});
+
+server.get('/me', (req, res) => {
+  res.json({name:'foo'})
+})
+
+server.on('uncaughtException',function(request, response, route, error){
+  console.error(error.stack);
+  response.send(error);
+});
+
+server.listen(port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
